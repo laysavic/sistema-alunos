1c84493e1db88be40cd4ede3698d44b53649f833
laysavic <laysa.alves8@aluno.ce.gov.br>
2025-09-24 21:19:02 -0300

    iniciando o create

---
+++ resources/views/alunos/create.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>cad-aluno</title>
+</head>
+<body>
+    <h1>Cadastrar aluno</h1>
+    
+    <form action="{{ route('alunos.store') }}" method="post">
+        @csrf
+        <input type="text" name="nome" >
+        <input type="text" name="matrícula">
+        <!-- <input type="text" name="curso">
+        <input type="number" name="idade"> -->
+        <input type="submit" value="cadastrar">
+
+    </form>
+</body>
+</html>
