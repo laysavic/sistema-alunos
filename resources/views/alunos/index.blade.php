b6b37d74d86c94151ee6a3a6349914d99b6ce82e
laysavic <laysa.alves8@aluno.ce.gov.br>
2025-09-24 21:19:23 -0300

    iniciando index

---
+++ resources/views/alunos/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alunos</title>
+</head>
+<body>
+    <h1>Alunos</h1>
+    <ul>
+        @foreach($alunos as $aluno)
+            <li>{{$aluno->nome}}</li>
+        @endforeach
+    </ul>
+</body>
+</html>
