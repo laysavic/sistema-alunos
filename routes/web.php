5ae2520bfafdf900a49f1b1aa35e61e028aaaf67
laysavic <laysa.alves8@aluno.ce.gov.br>
2025-09-24 21:19:40 -0300

    criando as rotas

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AlunoController;
 
 Route::get('/', function () {
     return view('welcome');
 
 
-
 });
+
+Route::get( '/alunos',[AlunoController::class, 'index'])->name('alunos.index');
+Route::get( '/alunos/create',[AlunoController::class, 'create'])->name('alunos.create');
+Route::post( '/alunos',[AlunoController::class, 'store'])->name('alunos.store');
+// Route::post( '/alunos/create',[AlunoController::class, 'create'])->name('alunos.create');
