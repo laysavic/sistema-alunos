01244fe4d8cf98aa7b1cf86b546d9c4be254f170
laysavic <laysa.alves8@aluno.ce.gov.br>
2025-09-26 23:28:53 -0300

    alterando as rotas

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,11 @@ Route::get('/', function () {
 
 });
 
-Route::get( '/alunos',[AlunoController::class, 'index'])->name('alunos.index');
-Route::get( '/alunos/create',[AlunoController::class, 'create'])->name('alunos.create');
-Route::post( '/alunos',[AlunoController::class, 'store'])->name('alunos.store');
-// Route::post( '/alunos/create',[AlunoController::class, 'create'])->name('alunos.create');
+// Route::get( '/alunos',[AlunoController::class, 'index'])->name('alunos.index');
+// Route::get( '/alunos/create',[AlunoController::class, 'create'])->name('alunos.create');
+// Route::post( '/alunos',[AlunoController::class, 'store'])->name('alunos.store');
+// Route::get( '/alunos/{aluno}/edit',[AlunoController::class, 'edit'])->name('alunos.edit');
+// Route::put( '/alunos/{aluno}/edit',[AlunoController::class, 'update'])->name('alunos.update');
+// Route::delete( '/alunos/{aluno}/edit',[AlunoController::class, 'destroy'])->name('alunos.destroy');
+
+Route::resource("/alunos", AlunoController::class);
