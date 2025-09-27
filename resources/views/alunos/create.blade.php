242135b03c50037c587d716555bf7c42b1a3ba44
laysavic <laysa.alves8@aluno.ce.gov.br>
2025-09-26 23:28:02 -0300

    desenvolvendo o create

--- resources/views/alunos/create.blade.php
+++ resources/views/alunos/create.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,12 +8,15 @@
 <body>
     <h1>Cadastrar aluno</h1>
     
-    <form action="{{ route('alunos.store') }}" method="get">
+    <form action="{{ route('alunos.store') }}" method="post">
         @csrf
         <label for="nome">Aluno: </label>
         <input type="text" name="nome" >
-        <input type="text" name="matrícula">
+        <label for="nome">Matrícula: </label>
+        <input type="text" name="matricula">
+        <label for="nome">Curso: </label>
         <input type="text" name="curso">
+        <label for="nome">Idade: </label>
         <input type="number" name="idade">
         <input type="submit" value="cadastrar">
 
