a17f2cb0d473925a2ad5ec3db5ac2625bf5ddcab
laysavic <laysa.alves8@aluno.ce.gov.br>
2025-09-26 23:27:37 -0300

    atualizando a migrate

--- database/migrations/2025_09_22_003625_create_alunos_table.php
+++ database/migrations/2025_09_22_003625_create_alunos_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('alunos', function (Blueprint $table) {
             $table->id();
             $table->string('nome');
-            $table->string('matricula')->unique();
+            $table->string('matricula');
             $table->string('curso');
             $table->integer('idade');
             $table->timestamps();
