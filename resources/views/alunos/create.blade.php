338ca653029a1341ea94baee012a2a5e13b6b8f9
laysavic <laysa.alves8@aluno.ce.gov.br>
2025-09-29 16:29:49 -0300

    estilizando create

--- resources/views/alunos/create.blade.php
+++ resources/views/alunos/create.blade.php
@@ -6,6 +6,75 @@
     <title>cad-aluno</title>
 </head>
 <body>
+     <style>
+        body {
+            font-family: 'Poppins', sans-serif;
+            background-color: #b2d7f0ff;
+            color: #5c42b1ff;
+            text-align: center;
+        }
+
+        h1 {
+            color: #2b3586ff;
+            margin-top: 40px;
+        }
+
+        .add-btn {
+            display: inline-block;
+            background-color: #1a005bff;
+            color: white;
+            padding: 10px 20px;
+            border-radius: 6px;
+            text-decoration: none;
+            margin-bottom: 20px;
+            font-weight: bold;
+        }
+
+        .add-btn:hover {
+            background-color: #8394ebff;
+        }
+
+        table {
+            margin: 0 auto;
+            border-collapse: collapse;
+            width: 90%;
+            box-shadow: 0 0 8px rgba(0,0,0,0.1);
+            background-color: #fff;
+        }
+
+        th, td {
+            padding: 12px;
+            border: 1px solid #403a4dff;
+        }
+
+        th {
+            background-color: #44469eff;
+            color: white;
+        }
+
+        td {
+            text-align: center;
+        }
+
+        .actions button {
+            background-color: #5543b1ff;
+            color: white;
+            border: none;
+            padding: 6px 10px;
+            border-radius: 5px;
+            cursor: pointer;
+            margin: 2px;
+        }
+
+        .actions button:hover {
+            background-color: #b4aeecff;
+        }
+
+        .actions a {
+            color: white;
+            text-decoration: none;
+        }
+    </style>
     <h1>Cadastrar aluno</h1>
     
     <form action="{{ route('alunos.store') }}" method="post">
