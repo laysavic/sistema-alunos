eaf0a24eb223ee8c72432dace2d938498e8e5773
laysavic <laysa.alves8@aluno.ce.gov.br>
2025-10-05 22:47:59 -0300

    adicionando required

--- resources/views/alunos/edit.blade.php
+++ resources/views/alunos/edit.blade.php
@@ -6,26 +6,27 @@
     <title>edt-aluno</title>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
+
 <body>
-     
+    
     <div class="cad-form">
         <h1>Editar aluno</h1>
-    
+
         <form action="{{ route('alunos.update', $aluno->id) }}" method="post">
             @method("put")
             @csrf
-            <label for="nome">Aluno: </label>
-            <input type="text" name="nome" value="{{ $aluno->nome }}">
+            <label for="nome">Aluno(a): </label>
+            <input type="text" name="nome" value="{{ $aluno->nome }}" required>
 
             <label for="nome">Matrícula: </label>
-            <input type="text" name="matricula" value="{{ $aluno->matricula }}">
+            <input type="text" name="matricula" value="{{ $aluno->matricula }}" required>
 
             <label for="nome">Curso: </label>
-            <input type="text" name="curso" value="{{ $aluno->curso }}">
+            <input type="text" name="curso" value="{{ $aluno->curso }}" required>
 
             <label for="nome">Idade: </label>
-            <input type="number" name="idade" value="{{ $aluno->idade }}">
-            
+            <input type="number" name="idade" value="{{ $aluno->idade }}" required>
+
             <button type="submit"> Altualizar </button>
 
         </form>
