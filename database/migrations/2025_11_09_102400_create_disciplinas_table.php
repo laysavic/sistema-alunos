4955e562490ef456aec2a0626779e4f3bfbdd762
laysavic <laysa.alves8@aluno.ce.gov.br>
2025-12-07 20:32:55 -0300

    definindo os tipos de cada coluna na migration de disciplinas

---
+++ database/migrations/2025_11_09_102400_create_disciplinas_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('disciplinas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->string('codigo')->nullable();
+            $table->string('carga_horaria')->nullable();
+            $table->foreignId('curso_id')->constrained('cursos')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('disciplinas');
+    }
+};
