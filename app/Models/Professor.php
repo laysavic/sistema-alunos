c4d46e9e8f3eb59f739488ee23c2361db8ac3feb
laysavic <laysa.alves8@aluno.ce.gov.br>
2025-12-07 20:25:30 -0300

    definindo os campos da tabela de professor

--- app/Models/Professor.php
+++ app/Models/Professor.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Professor extends Model
 {
-    //
+    protected $table = 'professores';
+    protected $fillable = ['nome', 'especialidade', 'email', 'telefone'];
 }
