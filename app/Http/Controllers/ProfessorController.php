91d04df52af0b0449232a11260eaacf13e433d52
laysavic <laysa.alves8@aluno.ce.gov.br>
2025-12-07 20:20:53 -0300

    iniciando as funções do controller de professores

--- app/Http/Controllers/ProfessorController.php
+++ app/Http/Controllers/ProfessorController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Professor;
 
 class ProfessorController extends Controller
 {
@@ -10,8 +11,9 @@ class ProfessorController extends Controller
      * Display a listing of the resource.
      */
     public function index()
-    {
-        //
+    {   
+        $professores = Professor::all();
+        return view("professores.index", compact('professores'));
     }
 
     /**
@@ -19,7 +21,7 @@ class ProfessorController extends Controller
      */
     public function create()
     {
-        //
+        return view("professores.create");
     }
 
     /**
@@ -27,15 +29,17 @@ class ProfessorController extends Controller
      */
     public function store(Request $request)
     {
-        //
-    }
+        $request->validate([
+            'nome' => 'required',
+            'especialidade' => 'required',
+            'email' => 'required|email',
+            'telefone' => 'required'
+        ]);
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
+        Professor::create($request->all());
+
+        return redirect()->route("professores.index")
+            ->with('success', 'Professor cadastrado com sucesso!');
     }
 
     /**
@@ -43,7 +47,8 @@ class ProfessorController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $professor = Professor::findOrFail($id);
+        return view("professores.edit", compact("professor"));
     }
 
     /**
@@ -51,7 +56,19 @@ class ProfessorController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $professor = Professor::findOrFail($id);
+
+        $request->validate([
+            'nome' => 'required',
+            'especialidade' => 'required',
+            'email' => 'required|email',
+            'telefone' => 'required'
+        ]);
+
+        $professor->update($request->all());
+
+        return redirect()->route("professores.index")
+            ->with('success', 'Professor atualizado com sucesso!');
     }
 
     /**
@@ -59,6 +76,92 @@ class ProfessorController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $professor = Professor::findOrFail($id);
+
+        $professor->delete();
+
+        return redirect()->route("professores.index")
+            ->with('success', 'Professor excluído com sucesso!');
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// <!-- <?php
+
+// namespace App\Http\Controllers;
+
+// use Illuminate\Http\Request;
+// use App\Models\Professor;
+
+// class ProfessorController extends Controller
+// {
+//     /**
+//      * Display a listing of the resource.
+//      */
+//     public function index()
+//     {
+//         //
+//     }
+
+//     /**
+//      * Show the form for creating a new resource.
+//      */
+//     public function create()
+//     {
+//         //
+//     }
+
+//     /**
+//      * Store a newly created resource in storage.
+//      */
+//     public function store(Request $request)
+//     {
+//         //
+//     }
+
+//     /**
+//      * Display the specified resource.
+//      */
+//     public function show(string $id)
+//     {
+//         //
+//     }
+
+//     /**
+//      * Show the form for editing the specified resource.
+//      */
+//     public function edit(string $id)
+//     {
+//         //
+//     }
+
+//     /**
+//      * Update the specified resource in storage.
+//      */
+//     public function update(Request $request, string $id)
+//     {
+//         //
+//     }
+
+//     /**
+//      * Remove the specified resource from storage.
+//      */
+//     public function destroy(string $id)
+//     {
+//         //
+//     }
+// } -->
