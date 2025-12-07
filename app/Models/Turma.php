ebd15ce70811efb88beeac9e4f674b6fcf3c9c50
laysavic <laysa.alves8@aluno.ce.gov.br>
2025-12-07 20:25:53 -0300

    definindo os campos da tabela de turma

--- app/Models/Turma.php
+++ app/Models/Turma.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Turma extends Model
 {
-    //
+    protected $fillable = [ 'nome','semestre','curso_id','disciplinas_id'];
+
+      public function curso()
+    {
+        return $this->belongsTo(Curso::class);
+    }
+
+    public function disciplina()
+    {
+        return $this->belongsTo(Disciplina::class, 'disciplinas_id');
+    }
 }
