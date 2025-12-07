d1eda0443a24ec5270a471cf1418c1baa70eee48
laysavic <laysa.alves8@aluno.ce.gov.br>
2025-12-07 20:27:21 -0300

    definindo os tipos de cada coluna na migration de cursos

--- database/migrations/2025_11_09_102356_create_cursos_table.php
+++ database/migrations/2025_11_09_102356_create_cursos_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('cursos', function (Blueprint $table) {
             $table->id();
+            $table->string('nome');
+            $table->text('descricao')->nullable();
+            $table->string('duracao')->nullable();
             $table->timestamps();
         });
     }
