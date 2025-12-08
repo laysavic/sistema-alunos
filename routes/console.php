2d794e458ed6477c9e83982c296e01a272d14d5a
laysavic <laysa.alves8@aluno.ce.gov.br>
2025-12-07 21:53:19 -0300

    apenas um teste

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,6 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+// <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/@4"></script>browser
